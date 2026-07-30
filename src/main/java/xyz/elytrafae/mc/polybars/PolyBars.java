2c8234192a2e3ffbd9dc72d0c9e64adaaf38e5b4
elytrafae <48065803+elytrafae@users.noreply.github.com>
2026-07-30 09:54:06 +0300

    Empty, but functional mod upload

---
+++ src/main/java/xyz/elytrafae/mc/polybars/PolyBars.java
@@ -0,0 +1,12 @@
+package xyz.elytrafae.mc.polybars;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PolyBars implements ModInitializer {
+
+    public static final String MODID = "polybars";
+
+    @Override
+    public void onInitialize() {
+    }
+}
